ce9243f6eb43d4de85c350c8e245bc9e459dd977
Kicshikxo <supermegavalera9000@gmail.com>
2025-10-03 00:49:25 +0300

    feat: basic drag and drop implementation

--- client/src/assets/js/index.js
+++ client/src/assets/js/index.js
@@ -1,1 +1,26 @@
 import '~/assets/css/pages/index.css'
+
+const uploadArea = document.querySelector('#upload-area')
+const fileUploadInput = document.querySelector('#file-upload-input')
+
+uploadArea.addEventListener('click', () => fileUploadInput.click())
+
+uploadArea.addEventListener('dragover', (e) => {
+  e.preventDefault()
+  uploadArea.classList.add('file-upload-container__upload-area--dragover')
+})
+
+uploadArea.addEventListener('dragleave', () => {
+  uploadArea.classList.remove('file-upload-container__upload-area--dragover')
+})
+
+uploadArea.addEventListener('drop', (e) => {
+  e.preventDefault()
+  uploadArea.classList.remove('file-upload-container__upload-area--dragover')
+  const files = e.dataTransfer.files
+  console.log(files)
+})
+
+fileUploadInput.addEventListener('change', (e) => {
+  console.log(e.target.files)
+})
